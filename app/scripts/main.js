4100556ff64a1276aab1c755f6c0e17975a7fd98
Nic Aitch <nic@nicinabox.com>
2013-12-22 15:34:05 -0600

    Update ui, sort by updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,21 @@
 $(function() {
-  $.getJSON('http://boiler-plugins-list.herokuapp.com', function(data) {
-    var plugins   = $("#tmpl-plugins").html();
+  $.getJSON('http://localhost:8011', function(data) {
+    var plugins   = $('#tmpl-plugins').html();
     var pluginsTemplate = Handlebars.compile(plugins);
 
     var search = $('#tmpl-search').html();
     var searchTemplate = Handlebars.compile(search);
 
+    _.each(data, function(i) {
+      i.updated_ago = moment(i.updated).fromNow()
+    });
+
+    data = _.sortBy(data, 'updated').reverse();
+
     $('form').html(searchTemplate({ count: data.length }));
     $('#plugins').html(pluginsTemplate({ plugins: data }));
 
-    var hackerList = new List('plugin-list', {
+    var pluginList = new List('plugin-list', {
       valueNames: [ 'name', 'description' ]
     });
   });
